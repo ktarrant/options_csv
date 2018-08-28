f364c29816dd347b2a89c4066c937d9b4d83523f
Kevin Tarrant <kmtarrant13@gmail.com>
2018-08-28 00:48:37 -0400

    add draft of leg model

--- journal/trades/models.py
+++ journal/trades/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Leg(models.Model):
+    BUY_OR_SELL_CHOICES = (("buy", "Buy"), ("sell", "Sell"),)
+    OPEN_OR_CLOSE_CHOICES = (("open", "Open"), ("close", "Close"),)
+    INSTRUMENT_CHOICES = (
+        ("call", "Call"),
+        ("put", "Put"),
+        ("stock", "Stock"),
+        ("fut", "Futures"),
+    )
+
+    symbol = models.CharField(max_length=16)
+    exec_date = models.DateTimeField("date executed")
+    buy_or_sell = models.CharField(
+        max_length=4,
+        choices=BUY_OR_SELL_CHOICES,
+        default="buy",
+    )
+    open_or_close = models.CharField(
+        max_length=5,
+        choices=OPEN_OR_CLOSE_CHOICES,
+        default="open",
+    )
+    instrument = models.CharField(
+        max_length=5,
+        choices=INSTRUMENT_CHOICES,
+        default="call",
+    )
+    quantity = models.PositiveIntegerField()
+    execution_price = models.FloatField()
+    execution_fees = models.FloatField(default=0)
+    expiration_date = models.DateTimeField("expiration", null=True)
+    margin = models.FloatField(null=True)
+    underlying_price = models.FloatField(null=True)
